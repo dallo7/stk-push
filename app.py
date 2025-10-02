5ace442eb230ce9cb676646a8a30f4583540e582
Dalmas Chituyi <122692086+dallo7@users.noreply.github.com>
2025-10-02 22:13:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def initiate_stk_push():
         passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
         business_short_code = '174379'
         process_request_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
-        callback_url = 'https://mydomain.com/patzh' # Remember to use ngrok for testing this
+        callback_url = 'https://beatbnk.onrender.com'
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         password = base64.b64encode((business_short_code + passkey + timestamp).encode()).decode()
         party_a = phone
